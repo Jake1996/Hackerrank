b5fa52e221eca40d59d0658545f06ba8a1d3dac6
Jake1996 <jatinpm96@gmail.com>
2017-06-20 20:56:21 +0530

    removed .class files

--- Equal.java
+++ Equal.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Equal {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        
+        int t = in.nextInt();
+        while(t--!=0) {
+            int n = in.nextInt();
+            int arr[] = new int[n];
+            for(int i=0;i<n;i++) {
+                arr[i] = in.nextInt();
+            }
+            Arrays.sort(arr);
+            
+        }           
         in.close();
     }
 }
