b708549d7d3d4dae35a0bf62f7d54709429d9923
Jake1996 <jatinpm96@gmail.com>
2017-09-07 20:25:46 +0530

    BoneTrousle

--- Bonetrousle.java
+++ Bonetrousle.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Bonetrousle {
@@ -5,22 +6,36 @@ public class Bonetrousle {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int q = in.nextInt();
-		long n,b,k;
-		while(q--!=0) {
-			n = in.nextLong();
-			k = in.nextLong();
-			b = in.nextLong();
-			long low = b*(b+1);
-			low/=2;
-			long up = 2*k*b-b*b+b;
-			up/=2;
-			if(n<low||n>up) {
+		BigInteger n, k, b;
+		while (q-- != 0) {
+			n = new BigInteger(in.nextLong() + "");
+			k = new BigInteger(in.nextLong() + "");
+			b = new BigInteger(in.nextInt() + "");
+			BigInteger low = b.multiply(b.add(BigInteger.ONE));
+			low = low.divide(BigInteger.valueOf(2));
+			BigInteger up = BigInteger.valueOf(2).multiply(k).multiply(b).subtract(b.multiply(b)).add(b);
+			up = up.divide(BigInteger.valueOf(2));
+			if (n.compareTo(low) < 0 || n.compareTo(up) > 0) {
 				System.out.println("-1");
+			} else {
+				StringBuilder sb = new StringBuilder();
+				long ans[] = new long[b.intValue()];
+				long add = (n.subtract(low)).divide(b).longValue();
+				long rem = (n.subtract(low)).mod(b).longValue();
+				for (int i = 0; i < b.intValue(); i++) {
+					ans[i] = i + add + 1;
+				}
+				if (rem != 0) {
+					for (int i = (int) (b.intValue() - rem); i < b.intValue(); i++) {
+						ans[i]++;
+					}
+				}
+				for (int i = 0; i < b.intValue() - 1; i++) {
+					sb.append(ans[i] + " ");
+				}
+				sb.append(ans[b.intValue() - 1]);
+				System.out.println(sb);
 			}
-			else {
-				
-			}
-			
 		}
 		in.close();
 	}
